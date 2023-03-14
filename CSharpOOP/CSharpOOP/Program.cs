1126eadfc5a603e29018d2f040ea8bbbd0fa60a2
Aayush <aayushbhatta000@gmail.com>
2023-03-14 00:35:26 -0500

    Created Objects in C#

---
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Warrior
+{
+    // Fields
+    public int height;
+    public int width;
+}
+
+public class Program
+{
+    static void Main()
+    {
+        Warrior theGoodGuy = new Warrior();
+        theGoodGuy.height = 190;
+        theGoodGuy.width = 80;
+
+        Warrior theBadGuy = new Warrior();
+        theBadGuy.height = 170;
+        theBadGuy.width = 100;
+
+        Console.WriteLine(theGoodGuy.height);
+    }
+}
